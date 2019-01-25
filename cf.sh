4336494ed6545abc3b3a632dd2f9e0dfa1352565
Kodak4400 <special.moucom@gmail.com>
2019-01-25 12:01:30 +0900

    [add]nullcheck

--- cf.sh
+++ cf.sh
@@ -11,19 +11,30 @@ if [ -f ${CONFIG} ] ; then
 
     pidx=`grep -n -w "^\[PARAMETER-OVERRIDES\]" ${CONFIG} | cut -d":" -f1`
     pidx_sft=`expr ${pidx} + 1`
-    OVERRIDLIST=`tail -n+${pidx_sft} ${CONFIG}|xargs`
-
-    echo "+++CONFIGパラメータの確認+++"
-    echo "STACKNAME = ${STACKNAME}"
-    echo "TEMPLATE = ${TEMPLATE}"
-    echo "PROFILE = ${PROFILE}"
-    echo "OVERRIDLIST = [${OVERRIDLIST}]"
-    echo "+++++++++++++++++++++++++"
+    OVERRIDELIST=`tail -n+${pidx_sft} ${CONFIG}|xargs`
 else
     echo "${CONFIG}が見つかりません"
     RETVAL=1
     exit
 fi 
+
+# NULLチェック
+NULL_FLG=0
+OVERRIDE_FLG=0
+if [ "${STACKNAME}" = "" ] ; then NULL_FLG=1 ; echo "[ERROR] STACKNAMEが設定されていません。" ; fi
+if [ "${TEMPLATE}" = "" ] ; then NULL_FLG=1 ; echo "[ERROR] TEMPLATEが設定されていません。" ; fi
+if [ "${PROFILE}" = "" ] ; then NULL_FLG=1 ; echo "[ERROR] PROFILEが設定されていません。" ; fi
+if [ "${OVERRIDELIST}" = "" ] ; then OVERRIDE_FLG=1 ; fi
+
+# NULLチェックの対象があればシェルを終了させる
+if [ ${NULL_FLG} -eq 1 ] ; then exit; fi
+
+echo "+++CONFIGパラメータの確認+++"
+echo "STACKNAME   = ${STACKNAME}"
+echo "TEMPLATE    = ${TEMPLATE}"
+echo "PROFILE     = ${PROFILE}"
+echo "OVERRIDLIST = [${OVERRIDELIST}]"
+echo "+++++++++++++++++++++++++"
 
 echo "*** deploy(d) or get UPDATE_FAILED event(g) or validation(v) or cancel(c)? [d/g/v/c]"
 read ANSWER
@@ -31,8 +42,11 @@ read ANSWER
 case ${ANSWER} in 
     [d/D] )
         echo "deploy start..."
-        #aws cloudformation deploy --stack-name ${STACKNAME} --template-file --capabilities CAPABILITY_IAM --profile ${PROFILE} --parameter-overrides ${PARAMS}
-        aws cloudformation deploy --stack-name ${STACKNAME} --template-file ${TEMPLATE} --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --profile ${PROFILE} --parameter-overrides ${OVERRIDLIST}
+        if [ ${OVERRIDE_FLG} -eq 1 ] ; then
+            aws cloudformation deploy --stack-name ${STACKNAME} --template-file ${TEMPLATE} --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --profile ${PROFILE}
+        else
+            aws cloudformation deploy --stack-name ${STACKNAME} --template-file ${TEMPLATE} --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --profile ${PROFILE} --parameter-overrides ${OVERRIDELIST}
+        fi
     ;;
     [g/G] )
         echo "get UPDATE_FAILED event..."
